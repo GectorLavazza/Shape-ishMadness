004c8e14f9d922796e1e161facd57dc5963f2a4d
pavlen5kiy <wolkpavel51@gmail.com>
2024-10-30 11:44:44 +0300

    Small changes

--- load_image.py
+++ load_image.py
@@ -10,17 +10,9 @@ def load_image(name, color_key=None, scale=(RATIO, RATIO)):
 
     try:
         image = pygame.image.load(fullname).convert_alpha()
-        image = pygame.transform.smoothscale_by(image, scale)
+        # image = pygame.transform.smoothscale_by(image, scale)
     except pygame.error as e:
         print(f"Err: {e}")
         raise SystemExit(e)
-
-    if color_key is not None:
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-
-    else:
-        image = image.convert_alpha()
 
     return image
