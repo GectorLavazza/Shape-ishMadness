e1aa43abe2b6a8d3b94f57547e3e9cc130b65186
pavlen5kiy <wolkpavel51@gmail.com>
2024-11-02 14:29:36 +0300

    Changes to SoundPlayer

--- sound.py
+++ sound.py
@@ -8,10 +8,15 @@ class SoundPlayer:
         self.volume = 0.5
         self.music_volume = 0.5
         self.music_set = False
+        self.count = 0
 
     def update(self, dt):
         if self.elapsed_time > 0:
             self.elapsed_time -= dt
+        else:
+            self.count = 0
+
+        print(self.elapsed_time, self.count)
 
     def set_music(self, filename):
         pygame.mixer.music.stop()
@@ -30,8 +35,9 @@ class SoundPlayer:
             if not volume:
                 v = self.volume
 
-            if self.elapsed_time <= 0:
+            if self.elapsed_time <= 0 and self.count <= 1:
                 sfx = pygame.mixer.Sound(f'assets/sfx/{filename}.wav')
                 sfx.set_volume(v)
                 sfx.play()
                 self.elapsed_time = self.timer
+                self.count += 1
