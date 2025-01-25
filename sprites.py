27ee239442894882cdd5111f814fb21eed863e2b
pavlen5kiy <wolkpavel51@gmail.com>
2025-01-25 22:19:10 +0300

    Small changes

--- sprites.py
+++ sprites.py
@@ -4,7 +4,3 @@ import pygame
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, *group):
         super().__init__(*group)
-
-    def rotate(self):
-        self.image = pygame.transform.rotate(self.image, 1)
-        self.rect = self.image.get_rect(center=self.rect.center)
